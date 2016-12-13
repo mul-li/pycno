fad231b3a6a5f13a2222dd9a68aa4857d43e49fa
Dennis Fink <dennis.fink@c3l.lu>
2016-12-14 00:03:55 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-
 from setuptools import setup, find_packages
 
 setup(
     name='pycno',
-    version='0.0.1-rc5',
+    version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires={
