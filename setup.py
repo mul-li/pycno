b50337c489a0cb288922747e9ab30a28adb3d989
Dennis Fink <dennis.fink@c3l.lu>
2017-01-19 21:15:50 +0100

    Fixed dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires={
-        'Flask==0.12.0',
+        'Flask==0.12',
         'Flask-WTF==0.14.2',
         'Pygments==2.1.3',
         'mulli==0.0.5'
