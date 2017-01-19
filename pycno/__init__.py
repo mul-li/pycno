c001a5bc9baa726a96726dc5bd4003bc51e68dfd
Dennis Fink <dennis.fink@c3l.lu>
2017-01-19 21:00:48 +0100

    Added celery

--- pycno/__init__.py
+++ pycno/__init__.py
@@ -4,6 +4,8 @@ import base64
 
 from flask import Flask
 
+from celery import Celery
+
 from mulli import HexConverter
 
 from .highlight import HighlightExtension
@@ -11,6 +13,8 @@ from .highlight import HighlightExtension
 import pkg_resources
 
 config_file = os.path.abspath('config.json')
+
+celery = Celery(__name__)
 
 
 def create_app():
@@ -26,6 +30,12 @@ def create_app():
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', _default_secret_key)
     app.config.setdefault('PYCNO_SITE_TITLE', 'Pycno')
     app.config.setdefault('DATABASE', 'pycno.pickle')
+    app.config.setdefault('PYCNO_CELERY_BROKER_URL', 'pyamqp://pycno:pycno@localhost:5672/pycno')
+
+    celery_config = app.config.get_namespace('PYCNO_CELERY_', lowercase=False)
+
+    from mulli import make_celery
+    make_celery(app, celery_config)
 
     app.url_map.converters['hex'] = HexConverter
     app.jinja_env.add_extension(HighlightExtension)
