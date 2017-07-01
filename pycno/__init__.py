2adec376130919624e1b4c97be49fc3258eb3669
Dennis Fink <dennis.fink@c3l.lu>
2017-07-01 03:39:44 +0200

    Updated mulli dependency

--- pycno/__init__.py
+++ pycno/__init__.py
@@ -6,15 +6,19 @@ from flask import Flask
 
 from celery import Celery
 
-from mulli import HexConverter
+import pkg_resources
+
+from mulli import Mulli
+from mulli import Celery as CeleryExt
 
 from .highlight import HighlightExtension
-
-import pkg_resources
 
 config_file = os.path.abspath('config.json')
 
 celery = Celery(__name__)
+
+celery_ext = CeleryExt()
+mulli_ext = Mulli(hex_converter=True)
 
 
 def create_app():
@@ -28,16 +32,17 @@ def create_app():
 
     _default_secret_key = base64.b64encode(os.urandom(32)).decode('utf-8')
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', _default_secret_key)
-    app.config.setdefault('PYCNO_SITE_TITLE', 'Pycno')
-    app.config.setdefault('DATABASE', 'pycno.pickle')
+
+    app.config['SITE_TITLE'] = app.config.get('PYCNO_SITE_TITLE', 'Pycno')
+    app.config['ONION_ADDRESS'] = app.config.get('PYCNO_ONION_ADDRESS', None)
+    app.config['DATABASE'] = app.config.get('PYCNO_DATABASE', 'pycno.pickle')
+
     app.config.setdefault('PYCNO_CELERY_BROKER_URL', 'pyamqp://pycno:pycno@localhost:5672/pycno')
-
     celery_config = app.config.get_namespace('PYCNO_CELERY_', lowercase=False)
+    celery_ext.init_app(app, celery_config)
 
-    from mulli import make_celery
-    make_celery(app, celery_config)
+    mulli_ext.init_app(app)
 
-    app.url_map.converters['hex'] = HexConverter
     app.jinja_env.add_extension(HighlightExtension)
 
     from .views import root_page
@@ -45,7 +50,11 @@ def create_app():
 
     @app.context_processor
     def inject_version():
-        return dict(PYCNO_VERSION=pkg_resources.require('pycno')[0].version)
+        return dict(APP_VERSION=pkg_resources.require('pycno')[0].version)
+
+    @app.context_processor
+    def inject_index_title():
+        return dict(INDEX_TITLE='Paste it!')
 
     return app
 
--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires={
-        'Flask==0.12',
+        'Flask==0.11.1',
         'Flask-WTF==0.14.2',
         'Pygments==2.1.3',
-        'mulli==0.0.5'
+        'mulli==0.0.8-dev'
     },
 )
